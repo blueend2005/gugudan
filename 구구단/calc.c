7a272290100b8717114dc4a3d121b0a549e1ddca
blueend2005@gmail.com <blueend2005@gmail.com>
2020-10-04 15:36:43 +0900

    구구단 소스코드

--- 구구단/calc.c
+++ 구구단/calc.c
@@ -6,37 +6,36 @@ int main()
 
 	int num1, num2;
 
-
 	printf("첫 번째 정숫값을 입력 : ");
 
-		scanf_s("%d", &num1);
+	scanf_s("%d", &num1);
 
-rewind(stdin);
+	rewind(stdin);
 
-printf("연산자 입력 (+,-,*, / 가능):");
+	printf("연산자 입력 (+,-,*, / 가능):");
 
-scanf_s("%c", &op_type, 1);
+	scanf_s("%c", &op_type, 1);
 
-printf("두 번째 정숫값을 입력 : ");
+	printf("두 번째 정숫값을 입력 : ");
 
-scanf_s("%d", &num2);
+	scanf_s("%d", &num2);
 
-switch (op_type)
-{
-case '+':
-	printf("%d + %d = %d\n", num1, num2, num1 + num2);
-	break;
-case '-':
-	printf("%d - %d = %d\n", num1, num2, num1 - num2);
-	break;
-case '*':
-	printf("%d * %d = %d\n", num1, num2, num1 * num2);
-	break;
-case '/':
-	if (num2 == 0) printf("오류가 발생했습니다! 0으로 나눌 수 없습니다!\n");
-	else printf("%d / %d = %.3f\n", num1, num2, num1 / (double)num2);
-	break;
-}
+	switch (op_type)
+	{
+	case '+':
+		printf("%d + %d = %d\n", num1, num2, num1 + num2);
+		break;
+	case '-':
+		printf("%d - %d = %d\n", num1, num2, num1 - num2);
+		break;
+	case '*':
+		printf("%d * %d = %d\n", num1, num2, num1 * num2);
+		break;
+	case '/':
+		if (num2 == 0) printf("오류가 발생했습니다! 0으로 나눌 수 없습니다!\n");
+		else printf("%d / %d = %.3f\n", num1, num2, num1 / (double)num2);
+		break;
+	}
 
-return 0;
+	return 0;
 }
